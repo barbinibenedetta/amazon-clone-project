38dc73e892e78b28f496bd99f847e7a1f728370c
barbinibenedetta <barbinibenedetta@gmail.com>
2024-08-13 21:24:50 +0200

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -83,3 +83,17 @@ export function updateQuantity(productId, newQuantity) {
 
   saveToStorage();
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingitem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingitem = cartItem;
+    }
+  });
+
+  matchingitem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
